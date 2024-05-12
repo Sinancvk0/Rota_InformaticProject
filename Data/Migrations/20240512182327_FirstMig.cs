d6f18b8078bd3d94c867efc1a286dfbc26017f35
Sinan ÇIVAK <sinancivak00@gmail.com>
2024-05-12 21:46:40 +0300

    Solution Admin added

---
+++ Data/Migrations/20240512182327_FirstMig.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tags = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Image2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Image3 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "References",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_References", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Services",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Services", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Solutions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Solutions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blogs");
+
+            migrationBuilder.DropTable(
+                name: "References");
+
+            migrationBuilder.DropTable(
+                name: "Services");
+
+            migrationBuilder.DropTable(
+                name: "Solutions");
+        }
+    }
+}
---
+++ Rota_InformaticProject/Areas/Admin/Controllers/SolutionController.cs
@@ -0,0 +1,99 @@
+﻿using Data.Abstract;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rota_InformaticProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SolutionController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SolutionController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetAll()
+        {
+
+            return Json(_unitOfWork.Solutions.GetAll().ToList());
+        }
+        [HttpGet]
+        public IActionResult SolutionAdd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SolutionAdd(Solution solution, IFormFile image)
+        {
+            if (image != null && image.Length > 0)
+            {
+
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", fileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    image.CopyTo(stream);
+                }
+
+
+                solution.Image = "/images/" + fileName;
+            }
+            _unitOfWork.Solutions.Add(solution);
+            _unitOfWork.Save();
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult SolutionDelete(Guid id)
+        {
+            _unitOfWork.Solutions.Remove(id);
+            _unitOfWork.Save();
+            return Ok();
+        }
+        [HttpGet]
+        public IActionResult GetServiceById(Guid id)
+        {
+            return Json(_unitOfWork.Solutions.GetById(id));
+        }
+        [HttpPost]
+
+        public IActionResult UpdateSolution(Solution solution, IFormFile image)
+        {
+            var existingService = _unitOfWork.Solutions.GetById(solution.Id);
+            if (existingService == null)
+            {
+                return NotFound();
+            }
+            if (!string.IsNullOrEmpty(solution.Title))
+            {
+                existingService.Title = solution.Title;
+            }
+            if (!string.IsNullOrEmpty(solution.Description))
+            {
+                existingService.Description = solution.Description;
+            }
+            if (image != null && image.Length > 0)
+            {
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", fileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    image.CopyTo(stream);
+                }
+                existingService.Image = "/images/" + fileName;
+            }
+            _unitOfWork.Solutions.Update(existingService);
+            _unitOfWork.Save();
+            return Ok();
+        }
+    }
+}
