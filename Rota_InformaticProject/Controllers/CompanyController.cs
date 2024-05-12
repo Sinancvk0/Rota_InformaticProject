d72f0ce1521204f5e47b91254dca04ba0839d680
Sinan ÇIVAK <sinancivak00@gmail.com>
2024-05-12 18:35:35 +0300

    About Added

---
+++ Rota_InformaticProject/Controllers/CompanyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rota_InformaticProject.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
