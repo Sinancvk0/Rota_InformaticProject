de8a3fb06d5cf3050cd11ecbb8ecdd6d888cc716
Sinan ÇIVAK <sinancivak00@gmail.com>
2024-05-14 16:13:21 +0300

    Blog Edit

--- Rota_InformaticProject/Controllers/BlogController.cs
+++ Rota_InformaticProject/Controllers/BlogController.cs
@@ -1,16 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Data.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Rota_InformaticProject.Controllers
 {
     public class BlogController : Controller
     {
+        private readonly IUnitOfWork _db;
+
+        public BlogController(IUnitOfWork db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(_db.Blogs.GetAll().ToList());
         }
-        public IActionResult BlogDetails()
+        public IActionResult BlogDetails(Guid id)
         {
-            return View();
+            var detail = _db.Blogs.GetById(id);
+            if (detail==null)
+            {
+                return NotFound();  
+
+            }
+            return View(detail);
         }
     }
 }
