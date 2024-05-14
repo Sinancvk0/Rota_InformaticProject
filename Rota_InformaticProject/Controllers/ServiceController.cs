f62eecb4dc8338dcaa280c980c2a11155417804d
Sinan ÇIVAK <sinancivak00@gmail.com>
2024-05-14 13:41:30 +0300

    service edit

--- Rota_InformaticProject/Controllers/ServiceController.cs
+++ Rota_InformaticProject/Controllers/ServiceController.cs
@@ -1,16 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Data.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Rota_InformaticProject.Controllers
 {
     public class ServiceController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ServiceController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(_unitOfWork.Services.GetAll().ToList());
         }
-        public IActionResult ServiceDetails()
+        public IActionResult ServiceDetails(Guid id)
         {
-            return View();
+            var detail=_unitOfWork.Services.GetById(id);
+            if (detail == null)
+            {
+                return NotFound();  
+            }
+
+            return View(detail);
         }
     }
 }
