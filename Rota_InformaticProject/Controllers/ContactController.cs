414714453b1ad8721fa81e00a777aa3f484d291c
Sinan ÇIVAK <sinancivak00@gmail.com>
2024-05-20 19:39:36 +0300

    .

--- Rota_InformaticProject/Controllers/ContactController.cs
+++ Rota_InformaticProject/Controllers/ContactController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using Rota_InformaticProject.Models;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Rota_InformaticProject.Controllers
 {
@@ -8,5 +11,39 @@ namespace Rota_InformaticProject.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Message(ContactViewModel contact)
+        {
+            if (ModelState.IsValid)
+            {
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress("Rota Yazılım İletişim", "sinancivak385@gmail.com"));
+                message.To.Add(new MailboxAddress("Alıcı Adı", "sinancivak00@gmail.com"));
+                message.Subject=contact.Subject;
+                message.Body = new TextPart("plain")
+                {
+                    Text = "FullName: " + contact.FullName + "\n" +
+                        "Email: " + contact.Email + "\n" +
+                        "Phone: " + contact.Phone + "\n" +
+                        "Subject: " + contact.Subject + "\n" +
+                        "Body: " + contact.Body
+                };
+
+                using (var client =new MailKit.Net.Smtp.SmtpClient())
+                {
+                    client.Connect("smtp.gmail.com", 587, false);
+                    client.Authenticate("sinancivak385@gmail.com", "abjmstmnmpzrvvno");
+                    client.Send(message);
+                    client.Disconnect(true);
+
+                }
+
+                return RedirectToAction("");
+            }
+
+            return View();
+
+        }
+
     }
 }
---
+++ Rota_InformaticProject/Models/ContactViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Rota_InformaticProject.Models
+{
+    public class ContactViewModel
+    {
+        public  required string FullName { get; set; }
+        public  required string Email { get; set; }
+        public  required string Phone { get; set; }
+        public  required  string Subject { get; set; }
+        public  required string Body { get; set; }
+    }
+}
