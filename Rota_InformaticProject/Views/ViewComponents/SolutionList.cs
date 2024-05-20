e685c62c0b95d1b72aed673eac01e7af0fea36b0
Sinan ÇIVAK <sinancivak00@gmail.com>
2024-05-21 00:37:28 +0300

    SolutionList Added

---
+++ Rota_InformaticProject/Views/ViewComponents/SolutionList.cs
@@ -0,0 +1,20 @@
+﻿using Data.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rota_InformaticProject.Views.ViewComponents
+{
+    public class SolutionList:ViewComponent
+    {
+        private readonly IUnitOfWork _db;
+
+        public SolutionList(IUnitOfWork db)
+        {
+            _db = db;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+              return View(_db.Solutions.GetAll().ToList());    
+        }
+    }
+}
