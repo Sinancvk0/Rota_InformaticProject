3f6ebc76763851738ed258cafef05b98e4a5ef29
Sinan ÇIVAK <sinancivak00@gmail.com>
2024-05-24 22:23:11 +0300

    Banner bacground

---
+++ Rota_InformaticProject/Controllers/ReferanceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rota_InformaticProject.Controllers
+{
+    public class ReferanceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
