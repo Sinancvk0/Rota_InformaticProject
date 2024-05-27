dec058b1335f3d0135ee8f36e59aefddda437f12
Sinan ÇIVAK <sinancivak00@gmail.com>
2024-05-27 23:06:19 +0300

    ReferanceController Getall Added

--- Rota_InformaticProject/Controllers/ReferanceController.cs
+++ Rota_InformaticProject/Controllers/ReferanceController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Data.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Rota_InformaticProject.Controllers
 {
     public class ReferanceController : Controller
     {
+        private readonly IUnitOfWork _db;
+
+        public ReferanceController(IUnitOfWork db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(_db.References.GetAll().ToList());
         }
     }
 }
