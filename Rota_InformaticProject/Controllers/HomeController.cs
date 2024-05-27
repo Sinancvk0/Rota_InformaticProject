eb5153c559237102964ce07d8f264180a2786d1d
Sinan ÇIVAK <sinancivak00@gmail.com>
2024-05-27 17:28:17 +0300

    .

--- Rota_InformaticProject/Controllers/HomeController.cs
+++ Rota_InformaticProject/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Data.Abstract;
 using Microsoft.AspNetCore.Mvc;
 using Rota_InformaticProject.Models;
 using System.Diagnostics;
@@ -6,10 +7,16 @@ namespace Rota_InformaticProject.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IUnitOfWork _db;
+
+        public HomeController(IUnitOfWork db)
+        {
+            _db = db;
+        }
 
         public IActionResult Index()
         {
-            return View();
+            return View(_db.Blogs.GetAll().ToList());
         }
 
     }
